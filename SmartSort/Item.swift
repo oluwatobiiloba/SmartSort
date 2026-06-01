a4660a85011c45f2d4df9a261dcc5114934b4586
Oluwatobiloba <oluwatobiloba.f.a@gmail.com>
2026-06-01 23:07:45 +0100

    Initial Commit

---
+++ SmartSort/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SmartSort
+//
+//  Created by Oluwatobiloba Aremu on 01/06/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SmartSortTests/SmartSortTests.swift
@@ -0,0 +1,19 @@
+//
+//  SmartSortTests.swift
+//  SmartSortTests
+//
+//  Created by Oluwatobiloba Aremu on 01/06/2026.
+//
+
+import Testing
+@testable import SmartSort
+
+struct SmartSortTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
